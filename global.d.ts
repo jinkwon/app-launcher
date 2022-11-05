f1689b579f499ce9509fdf6d9eeefd87fc62e6ce
jay.lee <uzmystic@gmail.com>
2022-11-06 08:01:13 +0900

    task: update sample codes

---
+++ global.d.ts
@@ -0,0 +1,31 @@
+/// <reference types="styled-components/cssprop" />
+
+declare module '*.svg' {
+  import * as React from 'react';
+
+  export const ReactComponent: React.FunctionComponent<React.SVGProps<SVGSVGElement> & { title?: string }>;
+
+  const src: string;
+  export default src;
+}
+
+declare module '*.html' {
+  const content: string;
+  export default content;
+}
+
+declare module '*.json' {
+  const content: any;
+  export default content;
+}
+
+interface CSSCustomProperties extends React.CSSProperties {
+  // Typescript 4.4 Template String Pattern Index Signature
+  [propName: `--${string}`]: string | number;
+}
+
+interface Window {
+  __BSTAGE_APP_SEND: any;
+}
+
+declare var window: Window;
---
+++ src/app-utils.ts
@@ -0,0 +1,26 @@
+
+/**
+ * intent 호출
+ * @param props
+ */
+export function moveToApp(props: {
+  stageId: string,
+}) {
+
+
+}
+
+/**
+ * app 에 json object 전달
+ */
+export function emit(type: string, payload: any) {
+  window?.__BSTAGE_APP_SEND?.(type, payload);
+  console.log(type, payload);
+}
+
+export function listener(cb: (data: any) => void) {
+  window.addEventListener('onmessage', (data) => {
+    console.log(data);
+    cb(data);
+  });
+}
